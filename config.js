c0c24bf0e32a791c4e1b70e805e01fbdd8f5de71
7ucg <161209670+7ucg@users.noreply.github.com>
2024-04-04 00:03:26 +0200

    update

---
+++ config.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const chalk = require("chalk");
+//
+global.prefa = ["."];                                    
+
+//
+global.Owner = ["4365022989060"];         
+global.OwnerNumber = [ "4365022989060"];   
+global.ownertag = [ "4365022989060"];
+global.OwnerName = "Baron";
+global.BotName = "Tool-BOT";
+global.packname = "By Tool Bot";                             //Do not change.
+global.author = "Baron";                               //Do not change.
+
+
+//
+global.location = "Deutschland, Unknown";
+global.reactmoji = "💞";
+global.themeemoji = "😂";
+global.websites = "https://baron.x10.bz";
+global.lolhuman = "Tool-BOT";
+
+
+//
+global.BotLogo = fs.readFileSync("./Assets/pic.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic.jpg");
+
+//
+global.mess = {
+  jobdone: '*Tadaaa✨!*',
+  useradmin: '*Dieser Befehl kann nur von einem Admin verwendet werden.* !',
+  botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+  botowner: 'Nur meine *Owner* können diesen Befehl benutzen!',
+  privateonly: 'Dieser Befehl ist nur für *Private Chats* Verfügbar!',
+  botonly: 'Only the *Bot itself* can use this command!',
+  waiting: '* ich arbeite daran...*',
+  error: 'Error!',
+  banned: 'You are *Banned* fron using commands!',
+  bangc: 'This Group is *Banned* from using Commands!',
+  
+}
