a78b6f506e473919a707305dcdd8cf74705e982e
7ucg <161209670+7ucg@users.noreply.github.com>
2024-04-30 03:42:17 +0200

    update

--- index.js
+++ index.js
@@ -1,100 +1,34 @@
 const cluster = require('cluster');
 const os = require('os');
-const path = require('path');
 const fs = require('fs');
 const yargs = require('yargs/yargs');
 const Readline = require('readline');
-const { spawn } = require('child_process');
 const CFonts = require('cfonts');
 const Winston = require('winston');
 
 
+const {
+  spawn
+} = require('child_process')
+const path = require('path')
 
-// Konfiguration des Loggers
-const logger = Winston.createLogger({
-  level: 'info',
-  format: Winston.format.combine(
-    Winston.format.timestamp(),
-    Winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
-  ),
-  transports: [
-    new Winston.transports.Console(),
-    new Winston.transports.File({ filename: 'server.log' })
-  ]
-});
-
-CFonts.say('Bot', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']
-});
-
-CFonts.say("Bot Original von Baron", {
-  font: 'console',
-  align: 'center',
-  gradient: ['red', 'magenta']
-});
-
-const numCPUs = os.cpus().length;
-let isRunning = false;
-
-/**
- * Startet eine JavaScript-Datei
- * @param {String} file Pfad/zu/Datei
- */
-function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-
-  const args = [path.join(__dirname, file), ...process.argv.slice(2)];
-  logger.info(`Starting process: ${[process.argv[0], ...args].join(' ')}`);
-
-  cluster.setupMaster({
-    exec: path.join(__dirname, file),
-    args: args.slice(1),
-  });
-
-  const worker = cluster.fork();
-
-  worker.on('message', data => {
-    logger.info(`[RECEIVED] ${data}`);
-    switch (data) {
-      case 'reset':
-        worker.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
-      case 'uptime':
-        worker.send(process.uptime());
-        break;
-    }
-  });
-
-  worker.on('exit', code => {
-    worker.kill();
-    isRunning = false;
-    start.apply(this, arguments);
-    isRunning = false;
-    logger.error(`Exited with code: ${code}`);
-    if (code !== 0) {
-      fs.watchFile(args[0], () => {
-        fs.unwatchFile(args[0]);
-        start(file);
-      });
-    }
-  });
-
-  const opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
-  if (!opts['test']) {
-    const rl = Readline.createInterface(process.stdin, process.stdout);
-    if (rl && rl.listenerCount() === 0) {
-      rl.on('line', line => {
-        worker.emit('message', line.trim());
-      });
-    }
-  }
+function start() {
+  let args = [path.join(__dirname, 'server.js'), ...process.argv.slice(2)]
+  console.log([process.argv[0], ...args].join('\n'))
+  let p = spawn(process.argv[0], args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+     })
+     .on('message', data => {
+        if (data == 'reset') {
+           console.log('Restarting Bot...')
+           p.kill()
+           start()
+           delete p
+        }
+     })
+     .on('exit', code => {
+        console.error('Exited with code:', code)
+        if (code == '.' || code == 1 || code == 0) start()
+     })
 }
-
-start('server.js');
-
-console.log(`Worker ${process.pid} started.`);
+start()
